abb67dc29a2ba4f381f326b06aeed249acdc6829
pttnguyet73 <pnguyet1992@gmail.com>
2024-10-12 10:19:34 +0700

    Update ComfirmFragment

---
+++ app/src/main/java/com/example/bansach/Fragment/ComfirmFragment.java
@@ -0,0 +1,19 @@
+package com.example.bansach.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bansach.R;
+
+public class ComfirmFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        android.view.View view = inflater.inflate(R.layout.activity_confirm, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/bansach/Fragment/OrderFragment.java
+++ app/src/main/java/com/example/bansach/Fragment/OrderFragment.java
@@ -1,14 +1,15 @@
 package com.example.bansach.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.bansach.R;
@@ -18,6 +19,21 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_order, container, false);
+
+        Button buy = view.findViewById(R.id.buy_button);
+
+        // Xử lý sự kiện nhấn nút
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ComfirmFragment newFragment = new ComfirmFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.container, newFragment) // Đảm bảo ID container đúng
+                        .addToBackStack(null) // Nếu muốn thêm vào backstack
+                        .commit();
+                Log.d("TAG", "đã có sự kiện ");
+            }
+        });
         return view;
     }
 }
