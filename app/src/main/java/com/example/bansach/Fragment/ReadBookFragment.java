1e2c50f0d0f2c4577fbdb8f0a08fd7dca7e393ca
Yen Nhu <luungocyennhu810@gmail.com>
2024-10-13 18:32:00 +0700

    Changed The Content Of The Book

--- app/src/main/java/com/example/bansach/Fragment/ReadBookFragment.java
+++ app/src/main/java/com/example/bansach/Fragment/ReadBookFragment.java
@@ -31,117 +31,96 @@ public class ReadBookFragment extends Fragment {
 
         // Đặt dữ liệu cho các view
         tvBookTitle.setText("Hồng lục");
-        tvSampleContent.setText("Chương 1: Nữ thần quay về rồi\n" +
+        tvSampleContent.setText("Chương 1: Nữ thần quay về rồi" +
+                "\n" + "Trên bàn cơm, qua ba tuần rượu, mọi người bắt đầu nhớ lại chuyện năm xưa.\n" +
                 "\n" +
-                "Tinh tinh tinh, nhóm đại học lâu lắm không có ai điểm danh đột nhiên trở nên sôi động trở lại, Hách Thúy Thúy vào xem, thì ra là Nhan Dịch Ngữ về nước. Cậu ta năm hai đại học xuất ngoại, đến nay được bốn năm rồi, nghe nói đã học xong thạc sĩ, bây giờ mang tiếng thơm về nước.\n" +
+                "Hách Thúy Thúy: Ngoại trừ Nhan Dịch Ngữ chuyển ngành rồi đi du học, những người khác mới chia tay hai năm, có gì mà hoài niệm?\n" +
                 "\n" +
-                "Trong nhóm sôi nổi đòi họp mặt làm tiệc tiếp đãi, bàn luận vui đến tung trời. Thậm chí có người ngay cả ảnh chụp chung với nữ thần cũng gửi vào trong nhóm. Hội ăn dưa kịch liệt lên án, nói anh ta ủ mưu, thế mà đã liên lạc riêng với nữ thần rồi!\n" +
+                "Nói mấy chuyện linh tinh thời học sinh, rồi mấy chuyện mờ ám trong công việc. Uống hơi phiêu phiêu, bắt đầu hỏi sang chuyện tư.\n" +
                 "\n" +
-                "Vị huynh đài gửi ảnh nọ vội vã giải thích, là gặp ở sân bay có được không hả!!\n" +
+                "Hách Thúy Thúy là phụ nữ đã có chồng, Khương Vân là con nhà tri thức, chỉ có Nhan Dịch Ngữ tính tình vừa mềm mại vừa thần bí, câu thứ nhất chính là hỏi tình hình yêu đương và hôn nhân của Nhan Dịch Ngữ.\n" +
                 "\n" +
-                "Hách Thúy Thúy chẳng quan tâm buổi họp mặt bạn bè sẽ được tổ chức ở đâu, thời gian lúc nào, cô nhìn chằm chằm vào người con gái rực rỡ như ánh hào quang, cái bụng nhỏ đầy phiền muộn!\n" +
+                "Nhan Dịch Ngữ nói không có một cách tự nhiên, mấy người đàn ông lập tức ồn ào, có người nói ánh mắt cô nàng cao, có người nói do quá quan trọng sự nghiệp, “Cũng nên suy nghĩ đi, lớn tuổi khó sinh con!”\n" +
                 "\n" +
-                "Nhìn người ta kìa! Ở nơi đất khác quê người xa lạ như vậy, mà vẫn giữ được tâm thái!\n" +
+                "“Chính là, phụ nữ không kết hôn không con cái vẫn là phụ nữ à?”\n" +
                 "\n" +
-                "Cô ném điện thoại, dẩu môi đi tìm thẻ tập gym của mình. Tìm được mấy tấm thì đều hết hạn cả rồi.\n" +
+                "Những người dẫn theo bạn gái hoặc vợ tới, không dám nói trắng trợn như thế, nhưng cũng cổ vũ: “Nhan mỹ nữ người theo đuổi nhiều quá chọn đến hoa mắt phải không?”\n" +
                 "\n" +
-                "Cô phiền não gửi tin nhắn qua cho bạn thân, muốn cô ấy theo mình đến phòng gym tập luyện. Khương Vân chả buồn phản ứng, chế nhạo cô lại muốn tặng tiền phòng tập gym à, Hách Đồng Quang nhà mi có biết mi đi nuôi đàn ông bên ngoài không?\n" +
+                "Nhan Dịch Ngữ khẽ cúi đầu vuốt tóc, nụ cười trên mặt không quá rõ ràng.\n" +
                 "\n" +
-                "Hách Thúy Thúy nằm ườn trên sô pha, cực kỳ mất hình tượng, cô thở dài một cách khoa trương, ta muốn người cũng như tên Khương Tiểu Vân à(*).\n" +
+                "Tinh thần chính nghĩa của Hách Thúy Thúy dâng cao, đang muốn mở miệng, điện thoại của Nhan Dịch Ngữ đột nhiên vang lên, cô nàng nhận điện thoại rồi nói: “Được, lập tức tới ngay.” Sau đó đứng dậy nói xin lõi, công ty có việc gấp cần giải quyết, bữa hôm nay là do cô nàng mời.\n" +
                 "\n" +
-                "((*)Ý bà chị là cũng muốn mình trở nên đẹp đẽ trong trẻo như ngọc phỉ thúy trong tên bà ý)\n" +
+                "Nói xong liền xách túi đi, để lại làn hương lượn lờ.\n" +
                 "\n" +
-                "Trước đây Khương Vân từng trêu tên cô, nói vừa nhìn thấy cô là muốn đội mũ cho cô rồi. Nhưng sau khi cô và Hách Đồng Quang kết hôn, Khương Vân không nhắc lại chuyện này nữa. Bỗng nhận ra gì đó, ông anh cô đội mũ cho cô sao? (*)\n" +
+                "Đàn ông trên bàn đều không kịp phản ứng, Hách Thúy Thúy lén lút hỏi Hách Đồng Quang: “Sao công ty không có gọi anh vậy?”\n" +
                 "\n" +
-                "(*) Ý là ông anh trai ngoại tình.\n" +
+                "Vừa rồi anh không cẩn thận nhìn thấy thông báo cuộc gọi đến trên màn hình di động của Nhan Dịch Ngữ, đang có chút nghi ngờ. Nghe vậy nhìn cô, “Em hi vọng anh đi tăng ca à?”\n" +
                 "\n" +
-                "Nữ chính hào quang vô hạn đã trở về, còn nữ phụ ăn no chờ chết sẽ có kết cục gì? Cô đau đớn ra sức uống trà, sau đó nghiến răng nghiến lợi đợi nam chính về nhà gây sự.\n" +
+                "Cùng với Nhan Dịch Ngữ? Thôi dẹp đi…\n" +
                 "\n" +
-                "Nam chính mặc sơ mi trắng, ồ, trên tay đã cầm cà vạt rồi, bây giờ đang tháo đồng hồ, hình như mới từ tiệc rượu nào đó về.\n" +
+                "Hách Thúy Thúy bĩu môi nói: “Em chỉ lo lắng anh thất nghiệp thôi, hừ hừ.”\n" +
                 "\n" +
-                "Đúng là cảnh đẹp ý vui!\n" +
+                "Hách Đồng Quang gắp rau cho cô, “Thất nghiệp vẫn nuôi được em, không cần lo.”\n" +
                 "\n" +
-                "Cô cho rằng lúc này, trong mắt nam chính sẽ đầy vẻ xem thường, căm ghét, châm biếm gì đó, nhưng hiển nhiên, cái người nhà cô không phải là một nam chính bình thường.\n" +
+                "Đàn ông uống rượu với nhau là bắt đầu khoác lác, cũng chẳng ai bận tâm đến cơm nước trên bàn, chỉ có cô với Khương Vân ăn uống no say, Khương Vân đẩy nhẹ cô, “Bàn này đắt lắm, ban đầu là định ăn cho hồi vốn, chẳng ngờ Nhan Dịch Ngữ chiêu đãi. Cậu nói xem có phải giờ cậu ta lắm tiền lắm không?”\n" +
                 "\n" +
-                "Anh cực kỳ bình tĩnh liếc qua TV, sau đó như một thói quen cà ràm một câu, nói xem ít mấy thứ thiểu năng thôi.\n" +
+                "“Chắc làm đầu tư đều lắm tiền?” Hách Thúy Thúy không hiểu, dù sao thì Hách Đồng Quang đầu tư tiền sinh tiền, bất kể là bản thân, hay là đầu tư vào con người.\n" +
                 "\n" +
-                "Tuy rằng phim truyền hình là thiểu năng, nhưng thực sự không phải cô vì quá đần mà! Có điều cô muốn nghĩ sao thì nghĩ, dù sao thì trong lòng anh ấy cô cũng không thể ngốc thêm được.\n" +
+                "Khương Vân gật gù trả lời, “Bây giờ sự nghiệp của cậu ta rất phát triển! Nữ cường nhân?”\n" +
                 "\n" +
-                "Hách Thúy Thúy chả quan tâm bĩu môi, tiếp tục rót trà.\n" +
+                "Nữ cường nhân? Hách Thúy Thúy nhét nửa miếng vịt nướng còn lại vào trong miệng, nghĩ thầm, nữ cường nhân này cùng với Hách Đồng Quang ngày ngày làm việc cùng nhau, không phải anh ấy sẽ càng tán thưởng cậu ta hơn sao?\n" +
                 "\n" +
-                "Mặc kệ Khương Vân oanh tạc trong wechat, Hách Thúy Thúy vẫn trước sau như một, ép cô ấy không thể không đồng ý ngày mai cùng Hách Thúy Thúy đi tìm phòng gym thích hợp.\n" +
+                "Vốn dĩ anh ấy đã rất tán thưởng cậu ta rồi!\n" +
                 "\n" +
-                "Lòng hiếu kỳ không được thỏa mãn, cuối cùng Khương Vân nhắn một tin độc ác tới, nguyền rủa cô cuộc sống sinh hoạt vợ chồng không được thỏa mãn!\n" +
+                "Hách Thúy Thúy nghiêng đầu nhìn, Hách Đồng Quang bị mấy tên con trai cầm cốc rượu vây quanh xin chỉ bảo mấy vấn đề liên quan đến cổ phiếu và ngân sách, hi vọng cho ít lời khuyên, tốt nhất chuẩn xác đến mức mua cái nào, bán cái nào…\n" +
                 "\n" +
-                "Hách Thúy Thúy rú lên một tiếng, bị Hách Đồng Quang kêu đi rủa mặt, tối đêm rồi còn gào thét cái gì vậy?\n" +
+                "“Giám đốc Hách, tôi còn mua cổ phiếu của anh đó.”\n" +
                 "\n" +
-                "Khương Vân nguyền rủa em không có cuộc sống vợ chồng như ý\n" +
+                "“Thế sao? Cảm ơn nhé.” Hách Đồng Quang khách sáo trả lời.\n" +
                 "\n" +
-                "Anh có chuyện gì liên quan đến cậu ta không đấy?\n" +
+                "“Ôi! Đừng khách sáo! Anh chính là người nhà mà, nhất định phải ủng hộ chứ!”\n" +
                 "\n" +
-                "Hách Thúy Thúy không nghe theo, chạy vào nhà vệ sinh bổ nhào lên lưng anh, anh nói em có hay không?\n" +
+                "Bên cạnh có người cầm điện thoại hỏi cổ phiếu Hách Đồng Quang quản lý là cái gì, người kia mở điện thoại ra đưa cho anh ta.\n" +
                 "\n" +
-                "Hách Đồng Quang đã sớm nhìn thấy hành động của cô qua gương, ôm cô một cái rồi bất đắc dĩ nhìn cô. Hách Thúy Thúy không biết xấu hổ níu cổ anh hôn một cái, cuối cùng, thành công có được một trải nghiệm cực kỳ mỹ mãn.\n" +
+                "Hách Thúy Thúy liếc mắt nhìn qua, tiền vốn 200………….\n" +
                 "\n" +
-                "Cô uốn éo cọ cọ người anh, Hách Đồng Quang đặt cô lên bồn rửa mặt, tay từ quần áo với vào bên trong, vừa sờ soạn vừa hôn cô, “Lại không mặc đồ lót…”\n" +
+                "“Ợ…” Hách Thúy Thúy che miệng, uống hai ngụm nước ngọt, nói muốn đi nhà vệ sinh. Khương Vân đang ăn cua, nói: “Cậu đi trước đi, tới theo sau ngay.”\n" +
                 "\n" +
-                "“Ở nhà không thích mặc…”\n" +
+                "Hách Thúy Thúy đi vệ sinh xong, lúc ở bồn rửa tay gặp phải một người.\n" +
                 "\n" +
-                "Xong chuyện, cô đắc ý nhắn cho Khương Vân một tin, “Vui sướng của lão nương nhà người muốn tưởng tượng cũng không nổi.”\n" +
+                "Đúng, họp lớp nhìn thấy bạn trai cũ là bình thường. Cô cũng có bạn trai cũ! Nhưng hai người chia tay không hòa bình lắm, trong lòng Hách Thúy Thúy không muốn nói chuyện với anh ta.\n" +
                 "\n" +
-                "Gửi xong tin nhắn Hách Thúy Thúy bị tịch thu điện thoại, bị nhắc nhở đi ngủ. Cô giả vờ ngoan ngoãn nằm ngủ, sau đó chui vào trong lòng chồng, luôn miệng mời gọi: “Anh trai yêu quý ơi, vừa rồi em thoải mái nhắm… anh ơi, anh thoải mái không… thịt em có phải rất mềm không…”\n" +
+                "Cô học nữ nhân vật phản diện trong phim truyền hình không coi ai ra gì, nhìn thấy cũng coi như không thấy, hất cằm lên cao, đang định rời đi, Trương Dụ đột nhiên mở miệng.\n" +
                 "\n" +
-                "Hách Đồng Quang không trả lời, anh biết nếu mà để ý đến cô, đêm nay khỏi mong yên ổn, nhưng Hách Thúy Thúy là người biết khó mà lui sao? Không, càng khó cô càng nhảy vào.\n" +
+                "“Cô chơi tôi đấy à?”\n" +
                 "\n" +
-                "“Anh trai yêu quý ơi, em bên dưới lại ướt rồi…”\n" +
+                "Hách Thúy Thúy: ???!!!\n" +
                 "\n" +
-                "Hách Đồng Quang nuốt một ngụm nước bọt, kêu lên một tiếng. Hai mắt Hách Thúy Thúy phát sáng, lập tức sà tới muốn ôm hôn, tách hai chân dán lên, dường như muốn chứng minh mình không nói dối.\n" +
+                "Hách Thúy Thúy: “Anh nói linh tinh gì thế? Giống như tôi với anh có giao dịch gì đó vậy.”\n" +
                 "\n" +
-                "“Em muốn anh tẫn tinh đến chết phải không?”\n" +
+                "Làm ơn đi! Cô là người vợ tốt người vợ trong sạch có được không?\n" +
                 "\n" +
-                "Hách Thúy Thúy cắn môi, bàn tay luồn vào trong quần áo ngủ sờ cơ bụng của anh, rì rầm nói, “Sao anh biết em muốn anh chết trên người em?”\n" +
+                "Trương Dụ đẩy kính, mắt kính phản xạ ánh đèn trên đỉnh đầu, lóe lên khiến Hách Thúy Thúy hơi sợ hãi.\n" +
                 "\n" +
-                "Hách Đồng Quang nghe xong, lập tức lật người đè cô, Hách Thúy Thúy lại được vui sướng rồi.\n" +
+                "Khương Tiểu Vân sao còn chưa đến? Hách Thúy Thúy hoảng sợ, nhà vệ sinh không một bóng người, quá giống hiện trường phạm tội?\n" +
                 "\n" +
-                "Cô cảm thấy Hách Đồng Quang vẫn rất có cảm giác với mình, vậy cô và Nhan Dịch Ngữ kia vẫn còn một trận đấu tranh thực lực.\n" +
+                "Cô nhìn khắp nơi, muốn tìm một món vũ khí thích hợp phòng thân, ai ngờ nghe thấy Trương Dụ hỏi, “Năm hai đại học cô đã kết hôn với Hách Đồng Quang rồi?”\n" +
                 "\n" +
-                "Nói đến đây, thật xấu hổ cô với Hách Đồng Quang hoàn toàn không phải kết hôn vì tình yêu!\n" +
+                "Hử? Hách Thúy Thúy không kịp thích ứng từ phim trường hành động sang phim trường tình cảm máu chó, đầu óc đần độn, “Là sao?”\n" +
                 "\n" +
-                "Hai người là rượu vào loạn tình bị bố mẹ bắt được…\n" +
+                "Bọn họ lúc đó không công khai, trừ những người bạn có quan hệ cực kỳ tốt ra, cũng không có nói cho người khác. Sau khi tốt nghiệp thì không cố ý che giấu, có người hỏi cô làm gì đó, cô đều nói đã kết hôn làm bà nội trợ.\n" +
                 "\n" +
-                "Hách Thúy Thúy che mặt.\n" +
+                "Cho nên hôm nay Trương Dụ mới biết.\n" +
                 "\n" +
-                "Đúng là đen đủi! Lần đầu đã…\n" +
+                "Nét mặt Trương Dụ từ tức giận sang phẫn nộ: “Tôi nói sao mà mỗi lần ra ngoài đều gặp phải Hách Đồng Quang, hai người dùng tôi làm chất xúc tác đúng không? Còn nói cái gì mà quan hệ anh em, có anh  em nhà ai mà kết hôn với nhau không?”\n" +
                 "\n" +
-                "Đương nhiên, lần thứ mấy ở trong mắt bố mẹ cũng không quan trọng. Hơn nữa năm đó ông nội nhặt được Hách Thúy Thúy đã tính toán muốn nuôi cô thành vợ cho cháu trai, tuy rằng mọi người không coi là thật, nhưng đưa đẩy thế nào, kết hôn lại thành thích hợp nhất.\n" +
+                "Hách Thúy Thúy đúng là oan uổng, “Lúc đó thật không có gì có được không!” Tuy rằng hồi cấp hai bọn họ có chút abcz, nhưng sau này lớn hơn thì đã hiểu chuyện, không có làm cái kia nữa! Hai người đều giả như không có chuyện gì xảy ra!\n" +
                 "\n" +
-                "Sau đó, hai người vừa tới tuổi kết hôn thì được bố mẹ dẫn đi đăng ký.\n" +
+                "Trương Dụ thấy cô gấp đến mức giậm chân chỉ cảm thấy buồn cười. Hai năm hai sáu tuổi đầu rồi còn ăn đồ hồng phấn, tay áo đính ren sau đầu cài nơ con bướm như một đứa ngốc. Giống như hồi đại học, một chút thay đổi cũng không có, anh ta cười chế giễu, “Ánh mắt của Hách Đồng Quang đúng là kém, lại đi kết hôn với cô.”\n" +
                 "\n" +
-                "Hách Thúy Thúy đồng ý một cách cực kỳ thoải mái, đương nhiên bởi vì chuyện này với cô chỉ có lợi không có hại. Đầu tiên, đạo đức và tính cách của Hách Đồng Quang cô rất rõ ràng, hai người ở chung không cần tốn thời gian làm quen. Quan trọng, bố mẹ chồng chính là bố mẹ mình rồi, cái này còn cần phải bàn sao! Cuối cùng, cô không cần phải đi làm.\n" +
+                "Hách Thúy Thúy gào lên, “Anh bị điên à! Mắt anh ấy tốt hơn mắt anh đó!” Cái gì cũng tốt hơn nhà mi!!!\n" +
                 "\n" +
-                "Cảm tạ trời đất, bởi cô chẳng có bản lĩnh gì, lại chịu không nổi khổ, Hách Đồng Quang chăm mạ (*) nhiều năm như vậy, cô vẫn là một gốc mạ vô dụng.\n" +
-                "\n" +
-                "(*)揠苗助长 ý là để mạ lớn nhanh nên kéo cây mạ lên để giúp nó, tương đương với câu “Giục tốc bất đạt”\n" +
-                "\n" +
-                "Trong nhà đã bàn bạc xong xuôi đợi cô kết hôn sẽ cho cô một ít bất động sản để cô cho thuê. Như vậy cô sẽ không quá khổ cực, cũng không cần nhìn sắc mặt nhà chồng để sống.\n" +
-                "\n" +
-                "Còn Hách Đồng Quang nghĩ như thế nào? Hách Thúy Thúy cũng từng hỏi qua, chủ yếu là trình bày những lợi ích của việc kết hôn, sau đó ngồi xổm trước mặt anh đáng thương nói: “Nếu anh thực không muốn kết hôn thì thôi vậy. Em đi nói với mẹ, không làm khó dễ anh nữa.”\n" +
-                "\n" +
-                "Hách Đồng Quang liếc cô một cái, như nhìn ra được tính toán nhỏ của cô: “Em nói?”\n" +
-                "\n" +
-                "Lúc đó trong lòng Hách Thúy Thúy nảy lên một cái, “Em… nếu ông nội biết… không biết ở dưới đó vui mừng đến thế nào.”\n" +
-                "\n" +
-                "Hách Đồng Quang cười lạnh một cái, “Nếu ông nội biết được chuyện em hồi năm nhất, sẽ không vui được đâu.”\n" +
-                "\n" +
-                "Hách Thúy Thúy chột dã, “Đó là đùa bỡn…”\n" +
-                "\n" +
-                "Cô thấy Hách Đồng Quang nhìn sách không để ý đến mình, sốt ruột: “Em với cậu ta còn chưa nắm tay! Em với anh còn ngủ với nhau rồi kìa! Tự anh đi tìm mẹ giải thích đi! Hừ!”\n" +
-                "\n" +
-                "Cô hậm hựm giậm chân rời đi.\n" +
-                "\n" +
-                "Sau đó bất an mấy ngày, cũng không thấy có biến cố gì, vẫn dựa theo kế hoạch đi đăng ký kết hôn.\n" +
-                "\n" +
-                "Còn Hách Đồng Quang, dường như không để tâm đến chuyện ngày đó, Hách Thúy Thúy tự nhiên còn quên nhanh hơn anh, tiếp tục trải qua những ngày không tim không phổi.");
+                "Năm đó đúng là cô bị mù rồi, mới cảm thấy hai người giống nhau!!!");
         imgBookCover.setImageResource(R.drawable.hong_luc);
 
         // Xử lý sự kiện khi người dùng nhấn nút "Mua sách"
